be9d9dd646a4f1eaa8b89607d22560b4fe5ad58b
Paulo Aguiar <aguiarlapaulo@gmail.com>
2023-11-17 11:21:00 -0300

    add products

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hi": "The Test is Perfect!"}
+
+products = [{
+    "id":1,
+    "name": "Iphone16",
+    "description": "cellphone",
+    "value": 5000
+},
+{
+    "id":2,
+    "name": "keyboard",
+    "description": "device",
+    "value": 50
+},
+{
+    "id":3,
+    "name": "mouse",
+    "description": "device",
+    "value": 50
+}]
+
+@app.get("/products")
+def product_list_return():
+    return products
 
 @app.get("/") #request
 def ola_mundo(): #response
